879fa39bf22c5070c977d72d266a2de0c5180a7e
Helton souza silveira <heltonss@dev.local>
2021-04-17 23:42:12 -0300

    feat: adiciona tela de login com links

--- lib/modules/login/login.dart
+++ lib/modules/login/login.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:time_is_money/modules/time_entry/time_entry_page.dart';
 
 class Login extends StatelessWidget {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final TextEditingController _email = TextEditingController();
   final TextEditingController _password = TextEditingController();
+
+  static Widget _containerInput(Widget input) {
+    return Container(
+      margin: const EdgeInsets.only(top: 20),
+      child: input,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,19 +26,58 @@ class Login extends StatelessWidget {
                 key: _formKey,
                 child: Column(
                     crossAxisAlignment: CrossAxisAlignment.end,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: form()))));
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: form(context)))));
   }
 
-  List<Widget> form() {
+  List<Widget> form(BuildContext context) {
     return <Widget>[
+      Container(
+        alignment: Alignment.topCenter,
+        margin: EdgeInsets.only(top: 50),
+        child: const Icon(Icons.access_time, size: 150, color: Colors.blueAccent,),
+      ),
+      Column(
+        children: inputs
+      ),
+      Padding(
+        padding: const EdgeInsets.symmetric(vertical: 30.0),
+        child: ConstrainedBox(
+          constraints: const BoxConstraints.tightFor(width: 150, height: 50),
+          child:  ElevatedButton(
+            onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) => TimeEntryPage() ));
+            },
+            child: const Text('acessar', style: TextStyle(fontSize: 16),)),
+      ),
+    ),
+      Container(
+        alignment: Alignment.center,
+        margin: const EdgeInsets.only(top: 30),
+        child:  TextButton(
+          onPressed: () {
+            acessar();
+          },
+          child: Text('criar usuario'.toUpperCase(),
+          style: const TextStyle(fontSize: 16),
+          ),
+        ),
+      )
+    ];
+  }
+
+
+  List <Widget> inputs = [
+    _containerInput(
       TextFormField(
         decoration: const InputDecoration(
           icon: Icon(Icons.email_outlined),
           hintText: 'Insira seu e-mail de login',
           labelText: 'e-mail',
         ),
-      ),
+       ),
+    ),
+    _containerInput(
       TextFormField(
         obscureText: true,
         obscuringCharacter: '*',
@@ -39,16 +87,11 @@ class Login extends StatelessWidget {
           labelText: 'password',
         ),
       ),
-      Padding(
-        padding: const EdgeInsets.symmetric(vertical: 30.0),
-        child: ElevatedButton(
-            onPressed: () {
-              acessar();
-            },
-            child: const Text('acessar')),
-      )
-    ];
-  }
+    )
+  ];
+
+
+
 
   void acessar() {
     print('acessando');
